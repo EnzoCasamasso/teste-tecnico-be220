e53ddc94f7f9247a7e4c11158a9785384ee97892
Enzo Casamasso <enzocasamasso@gmail.com>
2025-09-07 11:29:22 -0300

    Initial commit

---
+++ capacitor.config.ts
@@ -0,0 +1,9 @@
+import type { CapacitorConfig } from '@capacitor/cli';
+
+const config: CapacitorConfig = {
+  appId: 'io.ionic.starter',
+  appName: 'teste-tecnico-be220',
+  webDir: 'www'
+};
+
+export default config;
